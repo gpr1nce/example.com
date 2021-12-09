314ccb57306e2875152b501e8aff087fda392ec6
George Prince <prince.george35@gmail.com>
2021-12-09 09:24:13 -0600

    Posts functions restored

--- public/posts/view.php
+++ public/posts/view.php
@@ -1,7 +1,6 @@
 <?php
 include '../../core/db_connect.php';
 
-$content .="<p><a href=\"edit.php?id={$row['id']}\">Edit Post</a><br>";
 
 $input = filter_input_array(INPUT_GET);
 $slug = preg_replace("/[^a-z0-9-]+/", "", $input['slug']);
@@ -13,6 +12,7 @@ $stmt->execute([$slug]);
 $row = $stmt->fetch();
 $content .= "<h1>{$row['title']}</h1>";
 $content .="<p><a href=\"delete.php?id={$row['id']}\">Delete</a><br>";
+$content .="<p><a href=\"edit.php?id={$row['id']}\">Edit Post</a><br>";
 
 
 echo $content;
